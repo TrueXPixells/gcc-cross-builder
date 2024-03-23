0371dd21da992d73b3df3f333cf6d25617ff5035
TrueXPixells <68606364+TrueXPixells@users.noreply.github.com>
2024-03-23 15:25:13 +0300

    Update

--- build.sh
+++ build.sh
@@ -49,7 +49,7 @@ function installPackagesMac {
 #    brew update
 #    brew upgrade
 #    brew install --force coreutils bzip2 flex gperf intltool gdk-pixbuf pcre openssl libtool lzip make p7zip gnu-sed unzip libmpc isl gmp mpfr guile expat zlib gawk gzip
-    brew install gsed
+    brew install gsed guile
     PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
 }
 
